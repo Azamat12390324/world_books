1e8ce3e1df132d1e53871e8625db0f9e5949468a
azamat <azamatabdurashitov0@gmail.com>
2023-11-17 22:04:27 +0500

    edit_authors,delete_authors

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,14 @@
 from django import forms 
 from datetime import date
+from .models import Author
 
+
+
+class Form_edit_author(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = '__all__'
+        
 
 class Add_authors(forms.Form):
     first_name = forms.CharField(label="First name author")
@@ -9,3 +17,6 @@ class Add_authors(forms.Form):
                                     widget=forms.widgets.DateInput(attrs={"type" : date}))
     about = forms.CharField(label="About author")
     photo = forms.ImageField(label="Photo author")
+    
+    
+    
